a8d3e7c102703eacbfe97b2315593079cabbe809
erdaserhan <erhan209@gmail.com>
2024-08-29 22:40:33 +0200

    handleDeleteTodo function ok

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react"
 
 export default function TodoInput(props) {
-    const { handleAddTodos} = props
-
-    const [todoValue, setTodoValue] = useState('')
+    const { handleAddTodos, todoValue, setTodoValue} = props
 
     return (
         <header>
@@ -12,6 +10,7 @@ export default function TodoInput(props) {
             }} placeholder="Enter todo ..."/>
             <button onClick={() => {
                 handleAddTodos(todoValue)
+                setTodoValue('')
             }}>Add</button>
         </header>
     )
